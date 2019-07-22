028dccba72ff786b83b4f6142ee656b39029dab8
pkro <pkrombach@web.de>
2019-07-22 21:24:42 +0200

    basic menu listing and "logo" added

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from orders.models import MenuItem, MenuCategory
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Menu")
+    menuCategories = MenuCategory.objects.all()
+    menu = {}
+    for category in menuCategories:
+        menu[category.name] = MenuItem.objects.filter(menu_category__name=category.name)
+
+    context = {
+        'menu': menu,
+    }
+    return render(request, 'orders/menu.html', context)
 
 def cart(request):
     return HttpResponse("Shopping cart")
