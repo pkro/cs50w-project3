40e5eb295ab01610841df9da4fb4fe7c2bfab1ab
pkro <pkrombach@web.de>
2019-07-22 07:18:55 +0200

    addeed cart model and admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MenuCategory, MenuItems, Cart
+from .models import MenuCategory, MenuItem, Cart
 admin.site.register(MenuCategory)
-admin.site.register(MenuItems)
+admin.site.register(MenuItem)
 admin.site.register(Cart)
 
 
--- orders/models.py
+++ orders/models.py
@@ -9,7 +9,7 @@ class MenuCategory(models.Model):
         return f"{self.name}"
 
 # (Regular Pizza) Cheese, 1 topping, 2 toppings, (Subs) Cheese, italian etc
-class MenuItems(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=64)
     menu_category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -20,9 +20,10 @@ class MenuItems(models.Model):
         return f"{self.menu_category} - {self.name}, extra for {self.extra_for} Small/Normal: {self.price}/Large: {self.price_large}"
 
 # https://stackoverflow.com/questions/4910905/in-django-how-do-you-make-a-model-refer-to-itself
-# class Cart(models.Model):
-#     # customer_id
-#     item = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+class Cart(models.Model):
+    # customer_id
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    extra_for = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
 
     
     
