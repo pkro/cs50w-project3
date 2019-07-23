8265c75bffb7a350ea096c465d882dbe4bdf8f31
pkro <pkrombach@web.de>
2019-07-23 08:35:59 +0200

    redundant field names changed

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Regular Pizza, Sicilian Pizza, Subs, Salads...
 class MenuCategory(models.Model):
     name = models.CharField(max_length=64)
-    menu_order = models.IntegerField()
+    order = models.IntegerField()
 
     def __str__(self):
         return f"{self.name}"
@@ -12,14 +12,13 @@ class MenuCategory(models.Model):
 # (Regular Pizza) Cheese, 1 topping, 2 toppings, (Subs) Cheese, italian etc
 class MenuItem(models.Model):
     name = models.CharField(max_length=64)
-    # optional menu_category so toppings and stand-alone items are better represented.
-    menu_category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, blank=True, null=True)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, null=True)
     price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     price_large = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     extra_for = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'''{self.menu_category} - {self.name}
+        return f'''{self.category} - {self.name}
                     { ', extra for ' + self.extra_for.name if self.extra_for else ''}
                     , Small/Normal: {self.price}/Large: {self.price_large}'''
 
--- orders/views.py
+++ orders/views.py
@@ -7,7 +7,7 @@ def index(request):
     menuCategories = MenuCategory.objects.all()
     menu = {}
     for category in menuCategories:
-        menu[category.name] = MenuItem.objects.filter(menu_category__name=category.name)
+        menu[category.name] = MenuItem.objects.filter(category__name=category.name)
 
     context = {
         'menu': menu,
