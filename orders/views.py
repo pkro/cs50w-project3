76bec0564209a3594e8cac446009b9e1d9d27d6a
pkro <pkrombach@web.de>
2019-07-26 08:30:14 +0200

    put toppings in own context

--- orders/views.py
+++ orders/views.py
@@ -4,15 +4,19 @@ from orders.models import MenuItem, MenuCategory
 
 # Create your views here.
 def index(request):
-    categories = MenuCategory.objects.all()
+    categories = MenuCategory.objects.all().exclude(name="Toppings")
     menu = {}
     for category in categories:
         menu[category.name] = {
             'categoryId': category.id,
             'menuItems': MenuItem.objects.filter(category__name=category.name),
         }
+        
+    toppings = MenuItem.objects.filter(category__name="Toppings")
+
     context = {
         'menu': menu,
+        'toppings': toppings,
     }
     return render(request, 'orders/menu.html', context)
 
